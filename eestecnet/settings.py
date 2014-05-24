f31163fad6b736254691292fdf183d7d17b36edb
Sebastian Wozny <arpheno@gmail.com>
2014-05-24 19:05:38 +0200

    static root addeed

--- eestecnet/settings.py
+++ eestecnet/settings.py
@@ -87,7 +87,7 @@ TEMPLATE_DIRS = (os.path.join(BASE_DIR, 'templates'), )
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'serve', 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 STATICFILES_FINDERS = (
