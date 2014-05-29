5704ee0301188b19ff21f582deb94a82e996304b
Sebastian Wozny <arpheno@gmail.com>
2014-05-29 21:46:29 +0200

    added logging and gunicorn

--- eestecnet/settings.py
+++ eestecnet/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'members',
     'news',
     'account',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -50,7 +51,46 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
-
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': [],
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
 AUTH_USER_MODEL = 'account.Eestecer'
 ROOT_URLCONF = 'eestecnet.urls'
 
