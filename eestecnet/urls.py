94e74837cb50ed18bf800a6f83d4e696fc7066e4
Sebastian Wozny <arpheno@gmail.com>
2014-06-20 19:21:12 +0200

    URL concept reworked.

--- eestecnet/urls.py
+++ eestecnet/urls.py
@@ -7,7 +7,7 @@ from account.views import EestecerProfile, auth, out, new
 from eestecnet.settings import MEDIA_ROOT
 from events.models import Event, Application
 from events.views import EventDetail, ApplyToEvent, InternationalEvents
-from members.models import Member
+from members.models import Member, Team, Commitment
 from members.views import CommitmentList, TeamList, MemberDetail
 from news.models import Entry
 
@@ -18,21 +18,19 @@ urlpatterns = patterns('',
     # url(r'^$', 'eestecnet.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$',view,{"target": "news"}),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^news/$', ListView.as_view(model=Entry)),
     url(r'^news/(?P<pk>[-_\w]+)/$', DetailView.as_view(model=Entry)),
-    url(r'^events/$', InternationalEvents.as_view(), name='events'),
-    url(r'^events/(?P<pk>[-_\w]+)/$', EventDetail.as_view()),
-    url(r'^events/(?P<pk>[-_\w]+)/apply/$', ApplyToEvent.as_view()),
-    url(r'^cities/$', CommitmentList.as_view()),
-    url(r'^cities/(?P<pk>[-_\w]+)/$', DetailView.as_view(model=Member)),
-    url(r'^teams/$', TeamList.as_view()),
-    url(r'^(?P<slug>[a-zA-Z0-9-_]+)/$', MemberDetail.as_view()),
+    url(r'^events/$',  InternationalEvents.as_view(),  name='events'),
+    url(r'^teams/$', TeamList.as_view(),name='teams'),
+    url(r'^cities/$', CommitmentList.as_view(),name='cities'),
+    url(r'^events/(?P<slug>[-\w]+)/$', EventDetail.as_view(), name='event'),
+    url(r'^teams/(?P<slug>[-\w]+)/$', DetailView.as_view(model=Team), name='team'),
+    url(r'^cities/(?P<slug>[-\w]+)/$', DetailView.as_view(model=Commitment), name='city'),
+    url(r'^people/(?P<slug>[-\w]+)/$', EestecerProfile.as_view(), name='user'),
     url(r'^login/', auth, name='login'),
     url(r'^logout/', out, name='logout'),
     url(r'^register/', new, name='register'),
-    url(r'^people/(?P<slug>[\w.@+-]+)/$', EestecerProfile.as_view(), name='profile'),
-    url(r'^teams/(?P<pk>[-_\w]+)/$', DetailView.as_view(model=Member)),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root':MEDIA_ROOT}),
     )
