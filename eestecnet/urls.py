5c0f979ee35afbab86a55bcb6194a8e7a2cae218
Sebastian Wozny <arpheno@gmail.com>
2014-06-20 15:41:15 +0200

    temp commit

--- eestecnet/urls.py
+++ eestecnet/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
 import account
+from account.views import EestecerProfile, auth, out, new
 from eestecnet.settings import MEDIA_ROOT
 from events.models import Event, Application
 from events.views import EventDetail, ApplyToEvent, InternationalEvents
@@ -27,7 +28,10 @@ urlpatterns = patterns('',
     url(r'^cities/(?P<pk>[-_\w]+)/$', DetailView.as_view(model=Member)),
     url(r'^teams/$', TeamList.as_view()),
     url(r'^(?P<slug>[a-zA-Z0-9-_]+)/$', MemberDetail.as_view()),
-    url(r'^account/',include('account.urls')),
+    url(r'^login/', auth, name='login'),
+    url(r'^logout/', out, name='logout'),
+    url(r'^register/', new, name='register'),
+    url(r'^people/(?P<slug>[\w.@+-]+)/$', EestecerProfile.as_view(), name='profile'),
     url(r'^teams/(?P<pk>[-_\w]+)/$', DetailView.as_view(model=Member)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root':MEDIA_ROOT}),
