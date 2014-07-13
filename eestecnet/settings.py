60a474a0496a437b2f1ec9c8d1e805ac25e53744
Sebastian Wozny <arpheno@gmail.com>
2014-07-14 00:32:25 +0200

    80% map size

--- eestecnet/settings.py
+++ eestecnet/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 from settings_local import *
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -19,7 +18,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -126,12 +124,6 @@ WSGI_APPLICATION = 'eestecnet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
--- eestecnet/settings_local.py
+++ eestecnet/settings_local.py
@@ -1,3 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 DEBUG = True
 TEMPLATE_DEBUG = True
 SECRET_KEY = 'krrxqq_)5*4erfj0y9_sg=j(r3pfcr6b^2$&o8af#09mugi!*0'
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+}
