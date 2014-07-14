4b15988281b83e4f5f8844fd51b15df6ddff0f10
Sebastian Wozny <arpheno@gmail.com>
2014-07-14 18:34:43 +0200

    fixed a bug where you could name non lc members board.

--- members/admin.py
+++ members/admin.py
@@ -1,12 +1,9 @@
-from datetime import datetime
 from django import forms
 from django.contrib import admin
 
 # Register your models here.
 from django.contrib.admin.widgets import FilteredSelectMultiple
-from django.contrib.auth.models import User, Group, Permission
 from account.models import Eestecer
-from events.models import Event
 from members.models import Member, MemberImage
 
 
@@ -14,7 +11,7 @@ class MemberInline(admin.TabularInline):
     """ Inline Widget to display the Members of the Member with relevant information"""
     model = Member.members.through
     """ The model that is used"""
-    readonly_fields = ['name','number_of_events','last_event']
+    readonly_fields = ['name', 'number_of_events', 'last_event']
     """The fields we want to display"""
     exclude = ['eestecer']
     """Exclude eestecer, or we end up being able to change things which we should not"""
@@ -25,36 +22,49 @@ class MemberInline(admin.TabularInline):
     def name(self, instance):
         """ Returns the User's full name"""
         return instance.eestecer.first_name + instance.eestecer.last_name
-    def last_event(self,instance):
+
+    def last_event(self, instance):
         """Returns the last time a User went to an :class:`events.models.Event`"""
         return instance.eestecer.last_event()
-    def number_of_events(self,instance):
-        """Returns the amount of times a User participated in :class:`Events <events.models.Event>`"""
+
+    def number_of_events(self, instance):
+        """Returns the amount of times a User participated in :class:`Events <events
+        .models.Event>`"""
         return instance.eestecer.events_participated()
+
     def has_add_permission(self, request):
         """This is important so admins can't mess around"""
         return False
 
+
 class MemberForm(forms.ModelForm):
     class Meta:
-        widgets={'members': FilteredSelectMultiple('members',is_stacked=False)}
+        widgets = {'members': FilteredSelectMultiple('members', is_stacked=False)}
+
     def __init__(self, *args, **kwargs):
         """Make sure only members of the Member can get privileges"""
         super(MemberForm, self).__init__(*args, **kwargs)
-        self.fields['priviledged'].queryset = Eestecer.objects.filter(members__pk=self.instance.pk)
+        self.fields['priviledged'].queryset = Eestecer.objects.filter(
+            members__pk=self.instance.pk)
+        self.fields['board'].queryset = Eestecer.objects.filter(
+            members__pk=self.instance.pk)
+
     def clean(self):
         """Put all priviledged members local admin rights"""
         return self.cleaned_data
+
+
 class MemberImageInline(admin.TabularInline):
     model = MemberImage
 
+
 class MyMemberAdmin(admin.ModelAdmin):
     """A custom Admin displaying additional Member specific information"""
-    form=MemberForm
-    readonly_fields = ['member_count',]
+    form = MemberForm
+    readonly_fields = ['member_count', ]
     exclude = ['members']
-    filter_horizontal = ['board','priviledged']
-    inlines = [MemberImageInline, MemberInline,]
+    filter_horizontal = ['board', 'priviledged']
+    inlines = [MemberImageInline, MemberInline, ]
 
     def get_queryset(self, request):
         """If the modifying User is not an Admin, only show them their own Members"""
@@ -63,4 +73,5 @@ class MyMemberAdmin(admin.ModelAdmin):
             return qs
         return qs.filter(priviledged=request.user)
 
-admin.site.register(Member,MyMemberAdmin)
+
+admin.site.register(Member, MyMemberAdmin)
