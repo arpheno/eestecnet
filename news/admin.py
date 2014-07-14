b88276d7e0031d521c51f26d326fbe94b6ae0dff
Sebastian Wozny <arpheno@gmail.com>
2014-07-14 18:00:34 +0200

    fixed a bug where a person could author news entries for other people

--- news/admin.py
+++ news/admin.py
@@ -11,6 +11,18 @@ class MyEntryAdmin(admin.ModelAdmin):
         # Usually a User can only add his or her own Events
         return qs.filter(author__in=request.user.members.all())
 
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        """ A Local admin will only be able to create :class:`Event`s for
+         :class:`Member`s that he has privileges for. Admins still get to see all
+         events"""
+        if request.user.is_superuser:
+            return super(MyEntryAdmin, self).formfield_for_manytomany(db_field, request,
+                                                                      **kwargs)
+        if db_field.name == "author":
+            kwargs["queryset"] = request.user.priviledged.all()
+        return super(MyEntryAdmin, self).formfield_for_manytomany(db_field, request,
+                                                                  **kwargs)
+
 
 admin.site.register(Entry,MyEntryAdmin)
 
