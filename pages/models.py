3f88877a3960c3ae30e33c4b6f689613f4e2d544
Sebastian Wozny <arpheno@gmail.com>
2014-07-16 00:50:30 +0200

    added staticpagemodel

--- pages/models.py
+++ pages/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Page(models.Model):
     url = models.CharField(max_length=30)
     content = models.TextField()
+
+    def __unicode__(self):
+        return self.url
