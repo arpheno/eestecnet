6adf1c9d9921641601e4118da91b75fa4e1098d0
Sebastian Wozny <arpheno@gmail.com>
2014-10-08 21:44:05 +0200

    added rough about page

--- eestecnet/urls.py
+++ eestecnet/urls.py
@@ -44,6 +44,7 @@ urlpatterns = patterns(
     url(r'^mail-queue/$', include('mailqueue.urls')),
     url(r'^contact', TemplateView.as_view(template_name='enet/contact.html')),
     url(r'^activities/*$', TemplateView.as_view(template_name='enet/activities.html')),
+    url(r'^about/*$', TemplateView.as_view(template_name='enet/about.html')),
     url(r'^newsletter/$', newsletter, name='newsletter'),
     url(r'^materials/', include('elfinder.urls')),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
