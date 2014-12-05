89861256ed6f936474aaaee3657f1e022748ef74
Sebastian Wozny <arpheno@gmail.com>
2014-12-05 23:00:52 +0100

    only 5 news circle

--- news/views.py
+++ news/views.py
@@ -1,9 +1,13 @@
 
 # Create your views here.
-from django.views.generic import DetailView, ListView
+from django.views.generic import ListView
+
 from news.models import Entry
 
 
 class home(ListView):
     model = Entry
     template_name = 'enet/home.html'
+
+    def get_queryset(self):
+        return Entry.objects.order_by('-pub_date')[:5]
