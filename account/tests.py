46e32e8876f1f499326a52f1cb18df52c869beea
Sebastian Wozny <arpheno@gmail.com>
2014-12-26 20:47:40 +0100

    account tests

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+from teams.tests import EESTECMixin
+
+
+class CanLoginTestcase(EESTECMixin, TestCase):
+    def test_can_login(self):
+        c = Client()
+        response = c.post("/login/", {"username": "user@eestec.net", "password": "test"})
+        response = c.get("/")
+        assert (response.context['user'] == self.user)
