3fa42408cf2a3d9cfe9f89fbad06dba5f0ecfb30
Sebastian Wozny <arpheno@gmail.com>
2015-01-07 12:07:36 +0100

    membership functionality

--- news/admin.py
+++ news/admin.py
@@ -17,8 +17,8 @@ class MyEntryAdminForm(ModelForm):
 
 
 class MembershipAdmin(admin.ModelAdmin):
-    list_filter = ['user', ]
-    list_display = ('user', 'board', 'privileged', 'alumni')
+    list_filter = ['user', 'team']
+    list_display = ('user', 'team', 'board', 'privileged', 'alumni')
     list_editable = ('board', 'privileged', 'alumni')
     def has_add_permission(self, request):
         return request.user.is_superuser
