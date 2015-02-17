0c968292c454e5930add78be723c3058a56ca4d0
Sebastian Wozny <arpheno@gmail.com>
2015-02-18 00:56:41 +0100

    rest foundation

--- eestecnet/routers.py
+++ eestecnet/routers.py
@@ -4,6 +4,7 @@ from apps.account.viewsets import People, GroupViewset
 from apps.events.viewsets import Events
 from apps.news.viewsets import News, Memberships
 from apps.teams.viewsets import Cities
+from eestecnet.serializers import Permissions
 
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -13,3 +14,4 @@ router.register(r'events', Events)
 router.register(r'news', News)
 router.register(r'memberships', Memberships)
 router.register(r'groups', GroupViewset)
+router.register(r'permissions', Permissions)
--- eestecnet/serializers.py
+++ eestecnet/serializers.py
@@ -1,7 +1,20 @@
 import logging
 
+from django.contrib.auth.models import Permission
+from rest_framework import serializers, viewsets
+
+
 logger = logging.getLogger(__name__)
 
+
+class PermissionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Permission
+
+
+class Permissions(viewsets.ReadOnlyModelViewSet):
+    queryset = Permission.objects.all()
+    serializer_class = PermissionSerializer
 
 class AdminMixin(object):
     def get_serializer(self, *args, **kwargs):
