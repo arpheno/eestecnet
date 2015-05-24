a1636ccd15d7473e85c2e25f231c1556cc2a71db
Sebastian Wozny <arpheno@gmail.com>
2015-05-24 16:31:57 +0200

    serializers for conversion

--- eestecnet/settings/conf/rest.py
+++ eestecnet/settings/conf/rest.py
@@ -4,5 +4,6 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
