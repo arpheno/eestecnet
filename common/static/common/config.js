eec4c11c2ad9177570ab8055210a0ec38d2fdbb0
Sebastian Wozny <swozn@dolby.com>
2015-10-02 15:20:14 +0200

    Applied color scheme as per design team recommendation

--- common/static/common/config.js
+++ common/static/common/config.js
@@ -58,7 +58,59 @@ angular.module('eestec.common.config', [
         }]);
     }])
     .config(["$mdThemingProvider", function ($mdThemingProvider) {
+        $mdThemingProvider.definePalette('eestecred', {
+            "50": "#fde9ea",
+            "100": "#fabcbf",
+            "200": "#f78f94",
+            "300": "#f46a70",
+            "400": "#f1444c",
+            "500": "#ee1f28",
+            "600": "#d01b23",
+            "700": "#b3171e",
+            "800": "#951319",
+            "900": "#771014",
+            "A100": "#fabcbf",
+            "A200": "#f78f94",
+            "A400": "#f1444c",
+            "A700": "#b3171e"
+        });
+
+        $mdThemingProvider.definePalette('eestecgreen', {
+            "50": "#e6fdf1",
+            "100": "#b3f8d6",
+            "200": "#80f3bb",
+            "300": "#55eea4",
+            "400": "#2aea8d",
+            "500": "#00e676",
+            "600": "#00c967",
+            "700": "#00ad59",
+            "800": "#00904a",
+            "900": "#00733b",
+            "A100": "#b3f8d6",
+            "A200": "#80f3bb",
+            "A400": "#2aea8d",
+            "A700": "#00ad59"
+        });
+
+        $mdThemingProvider.definePalette('eestecgrey', {
+            "50": "#e9e9e9",
+            "100": "#bcbcbc",
+            "200": "#909090",
+            "300": "#6b6b6b",
+            "400": "#464646",
+            "500": "#212121",
+            "600": "#1d1d1d",
+            "700": "#191919",
+            "800": "#151515",
+            "900": "#111111",
+            "A100": "#bcbcbc",
+            "A200": "#909090",
+            "A400": "#464646",
+            "A700": "#191919"
+        });
+
         $mdThemingProvider.theme('default')
-            .primaryPalette('red')
-            .accentPalette('light-blue');
+            .primaryPalette('eestecred')
+            .accentPalette('eestecgreen');
+            //.backgroundPalette('eestecgrey');
     }]);
