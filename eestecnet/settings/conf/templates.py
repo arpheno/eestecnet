d025bea4b33494b05113523bbb9bacac92862a35
swozn <s.s.wozny@gmail.com>
2015-11-09 21:12:36 +0100

    Styled a sponser image

--- eestecnet/settings/conf/templates.py
+++ eestecnet/settings/conf/templates.py
@@ -4,13 +4,13 @@ from eestecnet.settings.conf.BASE_DIR import BASE_DIR
 
 
 TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
     "django.contrib.auth.context_processors.auth",
     "django.core.context_processors.debug",
     "django.core.context_processors.i18n",
     "django.core.context_processors.media",
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
-    "django.core.context_processors.request",
     "django.contrib.messages.context_processors.messages",
 )
 TEMPLATE_DIRS = (
