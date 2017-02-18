e4377b57db8cd93bb0b40339f45339ee0362d47b
swozn <arpheno@gmail.com>
2017-02-18 16:04:13 +0100

    Password reset isn't sensitive to trailing slashes anymore

--- apps/password_reset/urls.py
+++ apps/password_reset/urls.py
@@ -5,10 +5,10 @@ from . import views
 
 urlpatterns = patterns(
     '',
-    url(r'^recover/(?P<signature>.+)/$', views.recover_done,
+    url(r'^recover/(?P<signature>.+)/?$', views.recover_done,
         name='password_reset_sent'),
-    url(r'^recover/$', views.recover, name='password_reset_recover'),
-    url(r'^reset/done/$', views.reset_done, name='password_reset_done'),
-    url(r'^reset/(?P<token>[\w:-]+)/$', views.reset,
+    url(r'^recover/?$', views.recover, name='password_reset_recover'),
+    url(r'^reset/done/?$', views.reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<token>[\w:-]+)/?$', views.reset,
         name='password_reset_reset'),
 )
